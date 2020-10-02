7d0ad069b05363fd205fb04d42e20b2da88ddc77
Pruthvish Vaghela <45917456+pruthvish98@users.noreply.github.com>
2020-10-03 01:10:54 +0530

    Update maildemo.php

--- maildemo.php
+++ maildemo.php
@@ -15,8 +15,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'prjphp8866@gmail.com';                 // SMTP username
-    $mail->Password = '8866neel';                           // SMTP password
+    $mail->Username = '*****';                 // SMTP username
+    $mail->Password = '****';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
